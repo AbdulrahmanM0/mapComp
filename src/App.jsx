8f3b8bd37a242dc0daae4a850636c4787d36fb21
Abdulrahman Mohammed <109548830+AbdulrahmanM0@users.noreply.github.com>
2025-06-01 06:14:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import './App.css'
+import "leaflet/dist/leaflet.css"
+import { Icon } from 'leaflet'
+
+function App() {
+
+  let markIcon = new Icon({iconUrl:"/marker.png",iconSize: [38, 95]})
+  let handlePosition = () => {
+    if (navigator) {
+    console.log(navigator)
+    }
+  }
+
+
+  
+  return (
+
+<button onClick={handlePosition}>
+  get position
+</button>
+
+  )
+}
+
+export default App
